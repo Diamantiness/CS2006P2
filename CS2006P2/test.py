350b991d2ac6ec6f8d65bceafc9993623d3436a5
Joseph Macgowan <jm524@st-andrews.ac.uk>
2024-02-26 13:36:29 +0000

    Updated coverage and fixed test

---
+++ CS2006P2/test.py
@@ -0,0 +1,8 @@
+import unittest
+from test_inputvalidator import *
+from test_intricate_integer import *
+from test_iterator_mulitplication import *
+from test_multiplication import *
+
+if __name__ == '__main__':
+    unittest.main()
