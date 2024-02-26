689ddd039b0be24dff2479a78291b680dc9df867
Joseph Macgowan <jm524@st-andrews.ac.uk>
2024-02-26 13:48:28 +0000

    added tests

--- CS2006P2/test_iterator_mulitplication.py
+++ CS2006P2/test_iterator_mulitplication.py
@@ -35,9 +35,25 @@ class TestCommutativeProperty(unittest.TestCase):
         print("Test for non-associative property with n = 5 and alpha = 2 passed.")
 
     def test_has_intricate_peculiar_property(self):
-        # Test for n = 4, alpha = 3
+    # Test for n = 4, alpha = 3
         self.assertTrue(iterator_has_intricate_peculiar_property(4, 3))
         print("Test for peculiar property with n = 4 and alpha = 3 passed.")
+
+    def test_1_0_edge_case(self):
+    # Test for n = 1, alpha = 0
+        self.assertTrue(iterator_has_intricate_peculiar_property(1, 0))
+        print("Test for peculiar property with n = 1 and alpha = 0 passed.")
+
+    def test_5_0_edge_np_prop(self):
+    # Test for n = 5, alpha = 0
+        self.assertFalse(iterator_has_intricate_peculiar_property(5, 0))
+        print("Test for peculiar property with n = 5 and alpha = 0 passed.")
+
+    def test_no_pec_prop_normal(self):
+    # Test for n = 7, alpha = 5 (where alpha != n - 1)
+        self.assertFalse(iterator_has_intricate_peculiar_property(7, 5))
+        print("Test for peculiar property with n = 7 and alpha = 5 passed.")
+
 
 if __name__ == '__main__':
     unittest.main()
