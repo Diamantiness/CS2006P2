8668fbfa03eb9ff95b403cf46e4e5031dc66b37c
David <123179951+Diamantiness@users.noreply.github.com>
2024-03-02 18:19:44 +0000

    Commented IntricateInteger

--- CS2006P2/intricate_integer.py
+++ CS2006P2/intricate_integer.py
@@ -2,11 +2,12 @@ import math
 
 class IntricateInteger:
     def __init__(self, obj, n, alpha):
-        if not isinstance(n, int) or n <= 0:
+        #Ensures the modulus, multiplier and integer are valid
+        if not isinstance(n, int) or n <= 0: #Modulus must be positive
             raise ValueError("Modulus must be positive")
-        if not isinstance(alpha, int) or alpha < 0 or alpha >= n:
+        if not isinstance(alpha, int) or alpha < 0 or alpha >= n: #Multiplier must be both positive and smaller than the modulus
             raise ValueError("Multiplier  must be in the range [0, n-1]")
-        if not isinstance(obj, int) or obj < 0 or obj >= n:
+        if not isinstance(obj, int) or obj < 0 or obj >= n: #Integer must be both positive and smaller than the modulus
             raise ValueError("Input integer must be in the range [0, n-1] (For modulus)")
         
         self.object = obj
@@ -14,17 +15,19 @@ class IntricateInteger:
         self.alpha = alpha
     
     def __str__(self):
+        #Prints the intricate integer formatted as "<obj mod n | alpha>"
         return f"<{self.object} mod {self.n} | {self.alpha}>"
     
     def __mul__(self, other):
+        #Ensures both arguments are intricate integers which also share both the modulus and alpha
         if not isinstance(other, IntricateInteger):
             raise TypeError("Multiplication needs to be between 2 Intricate Integers")
         if self.n != other.n or self.alpha != other.alpha:
-            raise ValueError("Intricate Integers mneed the same modulus and alpha")
+            raise ValueError("Intricate Integers need the same modulus and alpha")
         
         lcm = math.lcm(self.object, other.object)
         result = (self.object + other.object + (self.alpha * lcm)) % self.n
-        return IntricateInteger(result, self.n, self.alpha)
+        return IntricateInteger(result, self.n, self.alpha) #Creates a new intricate integer as a result of the operation
     
 class IntricateIntegers:
     def __init__(self, n, alpha):
